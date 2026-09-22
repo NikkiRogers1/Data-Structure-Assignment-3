ff66118c9e7e6248476e4e8d1de62d76e2520576
NikkiRogers1 <nikkiharris1@student.bpcc.edu>
2026-09-21 20:21:27 -0500

    Phase 2 Complete

--- src/FileSystemAnalyzer.java
+++ src/FileSystemAnalyzer.java
@@ -14,4 +14,34 @@ for (FileSystemItem child : ((Folder) item).getItems()) {
 
 }
 
+public static int calculateTotalSizeRecursive(FileSystemItem item) {
+    if (item instanceof FileItem)  
+        return item.getSizeInKB();
+
+    int totalSize = 0;
+
+    for (FileSystemItem child : ((Folder) item).getItems()) {
+        totalSize += calculateTotalSizeRecursive(child);
+    
+    }
+    return totalSize;
+}
+
+public static FileItem findLargestFileRecursive(FileSystemItem item) {
+    if (item instanceof FileItem)
+        return (FileItem) item;
+
+    FileItem largestFile = null;
+
+    for (FileSystemItem child : ((Folder)item).getItems()){
+        FileItem childLargFile = findLargestFileRecursive(child);
+    if (childLargFile != null && 
+    (largestFile == null || childLargFile.getSizeInKB() > largestFile.getSizeInKB())) {
+    largestFile = childLargFile;
+}
+        
+    }
+
+    return largestFile;
+} 
 }
--- src/Main.java
+++ src/Main.java
@@ -11,8 +11,16 @@ public class Main {
         pictures.addItem(vacation);
         vacation.addItem(beach);
         vacation.addItem(renfair);
-
+       
         int fileCount = FileSystemAnalyzer.countFilesRecursive(root);
         System.out.println("Total files: " + fileCount);
+
+        int totalSize = FileSystemAnalyzer.calculateTotalSizeRecursive(root);
+        System.out.println("Total Size: " + totalSize);
+
+        FileItem largestFile = FileSystemAnalyzer.findLargestFileRecursive(root);
+        System.out.println("Largest File:" + largestFile.getName());
+        System.out.println("Largest File Size: " + largestFile.getSizeInKB() + " KB");
+
     }
 }
